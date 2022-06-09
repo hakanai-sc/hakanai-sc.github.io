144540d1765673602b6da71b194ccd6a77575bc5
hakanai-sc <hakanai-sc@users.noreply.github.com>
2022-06-08 22:57:23 -0700

    clean up share link colors

--- js/main.ts
+++ js/main.ts
@@ -1,11 +1,9 @@
 import Share from './controls/Share';
 
 document.addEventListener('DOMContentLoaded', () => {
-
   // Activate enhanced share panel interactivity
   const sharePanel = document.getElementById('sharePanel');
   if (sharePanel !== null) {
     const shareInstance = new Share(document.getElementById('sharePanel'));
   }
-
 });
